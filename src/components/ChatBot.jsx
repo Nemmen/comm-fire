a2c28a0b7d25f41a7076feaab799b2c84a76ebbf
SuperiorSU <sujalsinha2001@gmail.com>
2024-02-25 17:42:51 +0530

    chatbot

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,9 +1,78 @@
 import React, { useState } from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { TbMessageChatbot } from "react-icons/tb";
+import { MainContainer, ChatContainer, MessageList, Message,MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
 
 const ChatBot = () => {
   const [isOpen, setIsOpen] = useState(false);
+const apiKey = "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"
 
+  const [messages, setMessages] = useState([
+    {
+      message:"Hello, I am Hope!",
+      sender:"Hope"
+    }
+
+    
+  ])
+  const [typing, setTyping] = useState(false)
+
+  const sendHandle=async (message)=>{
+    const newMessage = {
+      message: message,
+      sender:"user",
+      direction: "outgoing"
+    }
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages)
+
+    setTyping(true)
+    await processMessage(newMessages)
+  }
+  async function processMessage(chatMessages){
+  let apiMessage = chatMessages.map((messageObject)=>{
+      let role = "";
+      if(messageObject.sender==="Hope"){
+        role = "assistant"
+      }
+      else{
+        role="user"
+      }
+      return {role:role, content: messageObject.message}
+    });
+    const systemMessage = {
+      role:"system",
+      content:"Explain all questions like a caring friend"
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages":[
+        systemMessage,
+        ...apiMessage
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions",{
+      method:"POST",
+      headers:{
+        "Authorization":"Bearer "+apiKey,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data)=>{
+      return data.json()
+    }).then((data)=>{
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          ender:"Hope"
+        }]
+      )
+      setTyping(false)
+    })
+  }
+  
   return (
     <div className="fixed bottom-6 right-6">
       <button
@@ -13,22 +82,25 @@ const ChatBot = () => {
         <TbMessageChatbot className="w-6 h-6" />
       </button>
       <div className={`fixed bottom-20 h-[400px] right-6 transition-opacity duration-300 ${isOpen ? 'opacity-100 bg-white rounded-lg shadow-lg' : 'opacity-0'}`}>
-        <div className="bg-white w-80    border border-gray-200">
+        <div className="bg-white w-80    border border-gray-200 h-[380px]">
           {/* Chatbot content goes here */}
           <div className="p-4">
-            <p className="text-lg font-semibold text-gray-800">Chatbot</p>
+            <p className="text-lg font-semibold text-gray-800">Hope</p>
             {/* Options based replies */}
             <div className="mt-4 space-y-2 ">
               {/* Example options */}
-              <p className='p-2 bg-white/50 '>Hey There! What You want to know!</p>
-              <div>
-                <button className="bg-purple-600 text-white px-4 py-2 rounded-lg">Option 1</button>
-              </div>
-              <div>
-                <button className="bg-purple-600 text-white px-4 py-2 rounded-lg">Option 1</button>
-              </div>
-              <div>
-                <button className="bg-purple-600 text-white px-4 py-2 rounded-lg">Option 1</button>
+              
+              <div className='relative '>
+                <MainContainer className='h-[390px]'>
+                  <ChatContainer className='h-[380px] '>
+                    <MessageList className='h-[380px] overflow-y-scroll' typingIndicator={typing?<TypingIndicator content="Hope is typing..."/>:null}>
+                      {messages.map((message,i)=>{
+                        return <Message key={i} model = {message} />
+                      })}
+                    </MessageList>
+                    <MessageInput placeholder='Enter your Query here!' onSend={sendHandle}/>
+                  </ChatContainer>
+                </MainContainer>
               </div>
             </div>
           </div>
